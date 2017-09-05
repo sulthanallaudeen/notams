936fb893fd1786d559fc82eb002a1c114d41800a
sulthanallaudeen <sa@sulthanallaudeen.com>
2017-09-05 12:39:45 +0530

    Showing Station Code in Form

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<style>
+#checkBoxLeft{
+    display:none;
+}
+</style>
+<div class="row">
+<div class="col-md-4">
+</div>
+<div class="col-md-4">
+<br>
+<?php
+if($_POST){
+    $code = $_POST['code'];
+    $code = strtoupper($code);
+}
+else{
+    $code = "";
+}
+?>
+<form method="POST" action="">
+  <div class="form-group">
+    <label for="email">ICAO Name:</label>
+    <input type="text" class="form-control" id="email" name="code" required value="<?php echo $code?>">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+</div>
+<div class="col-md-4">
+</div>
+</html>
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
@@ -17,26 +49,3 @@ else{
     //echo $error;
 }
 ?>
-<html>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<style>
-#checkBoxLeft{
-    display:none;
-}
-</style>
-<div class="row">
-<div class="col-md-4">
-</div>
-<div class="col-md-4">
-<br>
-<form method="POST" action="">
-  <div class="form-group">
-    <label for="email">ICAO Name:</label>
-    <input type="text" class="form-control" id="email" name="code" required>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
-</div>
-<div class="col-md-4">
-</div>
-</html>
